1110cb28cf529fd053aa733fc8bf5ad4d527e663
CodaBool <codabool@pm.me>
2026-06-08 19:22:54 -0400

    make current

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// Hooks.once('init', () => {
-// })
-
 import { mothership } from "./mothership.js"
 
 Hooks.once("ready", async () => {
@@ -12,24 +9,22 @@ Hooks.once("ready", async () => {
       payload.macroId &&
       typeof payload.macroId === "string"
     ) {
-      let macro = game.macros.get(payload.macroId);
+      const macro = await fromUuid(payload.macroId)
       if (!macro) {
-        console.log("macro not found by id attempting uuid");
-        macro = fromUuidSync(payload.macroId);
-        if (!macro) {
-          console.log("macro not found by id or uuid");
-          return;
-        }
+        console.log("macro not found by uuid")
+        return
       }
 
-      const command = macro.command;
+      const command = macro.command
       if (macro.type === "script") {
-        const AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
+        const AsyncFunction = Object.getPrototypeOf(
+          async function () {},
+        ).constructor
         const func = command.includes("await")
           ? new AsyncFunction("gmContext", command)
-          : new Function("gmContext", command);
+          : new Function("gmContext", command)
 
-        await func.call(globalThis, payload.gmContext || {});
+        await func.call(globalThis, payload.gmContext || {})
       }
     }
 
@@ -41,18 +36,21 @@ Hooks.once("ready", async () => {
       payload.flags &&
       typeof payload.flags === "object"
     ) {
-      if (!game.user.isGM) return;
+      if (!game.user.isGM) return
 
-      const doc = fromUuidSync(payload.uuid);
+      const doc = fromUuidSync(payload.uuid)
       console.log("set flag", doc, "flags", payload.flags)
       if (!doc) {
-        console.error("custom-foundry | document not found for uuid", payload.uuid);
-        return;
+        console.error(
+          "custom-foundry | document not found for uuid",
+          payload.uuid,
+        )
+        return
       }
 
-      const entries = Object.entries(payload.flags);
+      const entries = Object.entries(payload.flags)
       for (const [key, value] of entries) {
-        await doc.setFlag("custom-foundry", key, value);
+        await doc.setFlag("custom-foundry", key, value)
       }
     } else if (
       payload.action === "setOwnership" &&
@@ -60,25 +58,28 @@ Hooks.once("ready", async () => {
       payload.userIds &&
       payload.ownershipLevel
     ) {
-      if (!game.user.isGM) return;
-      const doc = fromUuidSync(payload.uuid);
+      if (!game.user.isGM) return
+      const doc = fromUuidSync(payload.uuid)
 
       if (!doc) {
-        console.error("custom-foundry | document not found for uuid", payload.uuid);
-        return;
+        console.error(
+          "custom-foundry | document not found for uuid",
+          payload.uuid,
+        )
+        return
       }
       const ownership = doc.ownership
       payload.userIds.forEach(id => {
         ownership[id] = payload.ownershipLevel
       })
       doc.update({ ownership })
-    } else if (
-      payload.action === "volumeQuery"
-    ) {
-      if (game.user.isGM) return;
+    } else if (payload.action === "volumeQuery") {
+      if (game.user.isGM) return
       let playing
       if (game.playlists.playing.length) {
-        playing = game.playlists.playing[0].sounds._source.find(p => p.playing)?.volume
+        playing = game.playlists.playing[0].sounds._source.find(
+          p => p.playing,
+        )?.volume
       }
       game.socket.emit("module.custom-foundry", {
         action: "log",
@@ -88,45 +89,38 @@ Hooks.once("ready", async () => {
           interface: game.settings.get("core", "globalInterfaceVolume"),
           playing,
         },
-        msg: `${game.user.name} | playlist: ${game.settings.get("core", "globalPlaylistVolume")} | ambient: ${game.settings.get("core", "globalAmbientVolume")} | interface: ${game.settings.get("core", "globalInterfaceVolume")} | playing: ${playing}`
-      });
-    } else if (
-      payload.action === "log"
-    ) {
+        msg: `${game.user.name} | playlist: ${game.settings.get("core", "globalPlaylistVolume")} | ambient: ${game.settings.get("core", "globalAmbientVolume")} | interface: ${game.settings.get("core", "globalInterfaceVolume")} | playing: ${playing}`,
+      })
+    } else if (payload.action === "log") {
       if (!game.user.isGM) return
       console.log(payload.data)
       ui.notifications.info(payload.msg)
     }
-  });
-
+  })
 
   // general code
   CONFIG.debug.hooks = false
   if (game.modules.get("custom-cursor").active) {
-    game.settings.set("custom-cursor", "defaultCursor", "codabool/img/heart/junk/40%5B0%5D.png")
-    game.settings.set("custom-cursor", "pointerCursor", "codabool/img/heart/junk/40%5B0%5D.png")
-    game.settings.set("custom-cursor", "grabCursor", "codabool/img/heart/junk/wii-open.png")
+    game.settings.set(
+      "custom-cursor",
+      "defaultCursor",
+      "codabool/img/heart/junk/cursor.png",
+    )
+    game.settings.set(
+      "custom-cursor",
+      "pointerCursor",
+      "codabool/img/heart/junk/cursor.png",
+    )
+    game.settings.set(
+      "custom-cursor",
+      "grabCursor",
+      "codabool/img/heart/junk/wii-open.png",
+    )
   }
 
   game.settings.set("core", "globalPlaylistVolume", 0.5)
   game.settings.set("core", "globalAmbientVolume", 0.5)
   game.settings.set("core", "globalInterfaceVolume", 0.5)
-
-
-  if (game.user.isGM) {
-    let fullUpdate = []
-    for (const playlist of game.playlists) {
-     	const updates = playlist.sounds.filter(s => s.volume !== 0.25 && !s.flags?.["custom-foundry"]?.volume)
-     	if (updates.length) fullUpdate.push(updates)
-    }
-    for (const playlists of fullUpdate) {
-      for (const playlist of playlists) {
-        await playlist.update({"volume": 0.25});
-      }
-    }
-    console.log("updated unmarked tracks to 0.25", fullUpdate.length)
-  }
-
 
   game.settings.register("custom-foundry", "ruler", {
     scope: "world",
@@ -139,11 +133,46 @@ Hooks.once("ready", async () => {
       if (value) {
         CONFIG.Token.rulerClass = null
       } else {
-        ui.notifications.info("reload to see token ruler again. May need to recreate tokens and combat")
+        ui.notifications.info(
+          "reload to see token ruler again. May need to recreate tokens and combat",
+        )
       }
-    }
+    },
   })
 
+  game.settings.register("custom-foundry", "volume", {
+    scope: "world",
+    name: "normalize music",
+    type: Boolean,
+    default: 0.2,
+    config: true,
+    restricted: true,
+  })
+
+  if (game.user.isGM) {
+    const volume = game.settings.get("custom-foundry", "volume")
+    let skipped = 0,
+      fullUpdate = []
+
+    for (const playlist of game.playlists) {
+      const updates = playlist.sounds.filter(
+        s => s.volume !== volume && !s.flags?.["custom-foundry"]?.volume,
+      )
+      if (updates.length) {
+        fullUpdate.push(updates)
+      } else {
+        skipped++
+      }
+    }
+    for (const playlists of fullUpdate) {
+      for (const playlist of playlists) {
+        await playlist.update({ volume: volume })
+      }
+    }
+    console.log(
+      `updated ${fullUpdate.length} tracks to ${volume}, skipped ${skipped}`,
+    )
+  }
 
   if (game.settings.get("custom-foundry", "ruler")) {
     CONFIG.Token.rulerClass = null
@@ -152,20 +181,38 @@ Hooks.once("ready", async () => {
   if (game.system.id === "mosh") await mothership()
 })
 
-
 Hooks.once("init", async () => {
+  // OBS specific
+  setTimeout(() => {
+    if (game.user.name === "OBS") {
+      document.querySelector("#fvtt-party-resources-status-bar")?.remove()
+    } else {
+      const bar = document.querySelector("#fvtt-party-resources-status-bar")
+      if (bar) bar.style.opacity = 0.3
+    }
+  }, 12_000)
+  Hooks.on("createChatMessage", async m => {
+    if (
+      typeof m?.flavor === "string" &&
+      m?.flavor.includes("rolls for Initiative!")
+    ) {
+      await m.delete()
+    }
+  })
   // hide some journals
-  Hooks.on('renderJournalDirectory', (app, htmlRaw) => {
+  Hooks.on("renderJournalDirectory", (app, htmlRaw) => {
     let html = htmlRaw
     if (game.release.generation === 12 || html?.length === 1) {
       html = html[0]
     }
-    const j = game.journal._source.filter(f => f.flags?.["custom-foundry"]?.hidden)
+    const j = game.journal._source.filter(
+      f => f.flags?.["custom-foundry"]?.hidden,
+    )
     if (j.length && !game.user.isGM) {
       j.forEach(entry => {
         const el = html.querySelector(`li[data-entry-id="${entry._id}"]`)
         if (el) el.remove()
       })
     }
-  });
+  })
 })
